e4826c9f4274ddbe2a62eb9351d7d8e90fe49b3c
jessikim <jessica.kim17@gmail.com>
2022-10-25 14:18:40 -0500

    fix wishlistform error

--- src/components/WishlistForm/WishlistForm.jsx
+++ src/components/WishlistForm/WishlistForm.jsx
@@ -1,32 +1,18 @@
 import styles from "./WishlistForm.module.css"
-import { useState } from "react"
-import { useLocation } from "react-router-dom"
 
 const WishlistForm = (props) => {  
-  const { state } = useLocation()
-  const [wishlistForm, setWishlistForm] = useState(state)
-
-  const handleChange = ({ target }) => {
-    setWishlistForm({ ...wishlistForm, [target.name]: target.value })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-		props.handleUpdateWishlist(wishlistForm)
-  }
-
   return ( 
     <>
-      <form onSubmit={handleSubmit} className={styles.itemForm}>
+      <form onSubmit={props.handleSubmit} className={styles.itemForm}>
         <label htmlFor="name-input">Name</label>
         <input
           required
           type="text"
           name="name"
           id="name-input"
-          value={wishlistForm.name}
+          value={props.wishlistForm.name}
           placeholder="Name"
-          onChange={handleChange}
+          onChange={props.handleChange}
         />
         <label htmlFor="description-input">Description</label>
 				<textarea
@@ -34,9 +20,9 @@ const WishlistForm = (props) => {
           type="text"
           name="description"
           id="description-input"
-          value={wishlistForm.description}
+          value={props.wishlistForm.description}
           placeholder="Description"
-          onChange={handleChange}
+          onChange={props.handleChange}
         />
         <button type="submit">SUBMIT</button>
       </form>
--- src/pages/EditWishlist/EditWishlist.jsx
+++ src/pages/EditWishlist/EditWishlist.jsx
@@ -1,10 +1,24 @@
 import styles from './EditWishlist.module.css'
 import WishlistForm from '../../components/WishlistForm/WishlistForm'
+import { useState } from "react"
+import { useLocation } from "react-router-dom"
 
 const EditWishlist = (props) => {
+  const { state } = useLocation()
+  const [wishlistForm, setWishlistForm] = useState(state)
+
+  const handleChange = ({ target }) => {
+    setWishlistForm({ ...wishlistForm, [target.name]: target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+		props.handleUpdateWishlist(wishlistForm)
+  }
+
   return (
     <main className={styles.container}>
-      <WishlistForm handleUpdateWishlist={props.handleUpdateWishlist} />
+      <WishlistForm handleUpdateWishlist={props.handleUpdateWishlist} wishlistForm={wishlistForm} handleChange={handleChange} handleSubmit={handleSubmit} />
     </main>
   )
 }
