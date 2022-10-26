604d354fb5a28eec82ac8675fc4b141ec474e2d4
chenannchi <anchichenusa@gmail.com>
2022-10-25 20:20:33 -0400

    modify the layout of landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,10 +5,8 @@ const Landing = ({ user }) => {
     <main className={styles.page}>
       <div className={styles.container}>
         <div className={styles.welcomeStatement}>
-          <div>
             Welcome to Giftology,<br />
             {user ? user.name : 'friend'}!
-          </div>
         </div>
         <div className={styles.introduction}>
           <p>Giftology helps you gift in confidence!</p>
