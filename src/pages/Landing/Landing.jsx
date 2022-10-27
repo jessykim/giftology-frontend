9ff4eae61328750e88dfb9c51fd20784a243fa98
jessikim <jessica.kim17@gmail.com>
2022-10-27 17:35:04 -0500

    add new images and finish about section

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,8 +4,6 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.page}>
       <section className={styles.first}>
-        {/* <img src='/landing2.jpg' alt='landing page'/> */}
-        {/* <div className={styles.container}> */}
           <div className={styles.welcome}>
             <div className={styles.statement}>
               Welcome to Giftology,<br />{user ? user.name : 'friend'}!
@@ -17,13 +15,15 @@ const Landing = ({ user }) => {
               We're here to help you gift in <span>confidence</span>!
             </div>
           </div>
-        {/* </div> */}
       </section>
       <section className={styles.second}>
-        <h2>About Us</h2>
-        <div>
-          <p>Some feel confident when it comes to finding the perfect gift for others, while others do not. We're here to help you gift with ease and feel confident when deciding on what gifts to buy. Giftology can help facilitate the communication between gift givers and receivers. You will be able to create your own wishlists as well as view your friends' wishlists to know exactly what they need or want for the next special occasion.</p>
-          <p>Giftology, where gifting is <span>MadeEz</span>!</p>
+        <div className={styles.about}>
+          <div className={styles.aboutTitle}>
+            <h2>About</h2>
+            <img src="./giftology3.png" alt="giftology" id={styles.cursive} />
+          </div>
+          <p className={styles.text}>Some feel confident when it comes to finding the perfect gift for others, while others do not. We're here to help you gift with ease and feel confident when deciding on what gifts to buy. Giftology can help facilitate the communication between gift givers and receivers. You will be able to create your own wishlists as well as view your friends' wishlists to know exactly what they need or want for the next special occasion.</p>
+          <img src="./note2.png" alt="company note" id={styles.note} />
         </div>
       </section>
     </main>
